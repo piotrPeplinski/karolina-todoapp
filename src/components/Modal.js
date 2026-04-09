a487dbbff9b5be5459ffa610ae231700a3bb5edf
Karo <karolina.szary93@gmail.com>
2026-04-09 14:09:46 +0200

    added modal and styled

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,16 +1,37 @@
-const Modal = ({ children, onClose, isOpen }) => {
+const Modal = ({ isOpen, onClose, children }) => {
   return (
     <>
       {isOpen ? (
-        <div className="modal">
-          <button onClick={onClose}>Close</button>
-          <div className="modal-content">{children}</div>
+        <div className="modal-overlay">
+          <div className="modal">
+            <ion-icon
+              className="closing-x"
+              name="close-outline"
+              onClick={onClose}
+            ></ion-icon>
+            <div className="modal-content">{children}</div>
+          </div>
         </div>
-      ) : (
-        null
-      )}
+      ) : null}
     </>
   );
 };
 
 export default Modal;
+
+// const Modal = ({ children, onClose, isOpen }) => {
+//   return (
+//     <>
+//       {isOpen ? (
+//         <div className="modal">
+//           <button onClick={onClose}>Close</button>
+//           <div className="modal-content">{children}</div>
+//         </div>
+//       ) : (
+//         null
+//       )}
+//     </>
+//   );
+// };
+
+// export default Modal;
--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,15 +1,29 @@
+import { useState } from "react";
+import { priorities } from "../utils/constants";
+import Modal from "./Modal";
+
 const TaskCard = ({ task }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsModalOpen(true);
+  };
+
   return (
     <div className="task-card">
-      <p>Task: {task.title}</p>
-      <p>Description: {task.desc || "No description"}</p>
-      <p>Completed: {task.completed ? "Yes" : "No"}</p>
-      <p>
-        Priority:{" "}
-        {task.priority === 3 ? "High" : task.priority === 2 ? "Medium" : "Low"}
-      </p>
-      <p>Created: {task.created_date.toLocaleDateString()}</p>
-      <p>Complete until: {task.complete_until_date.toLocaleDateString()}</p>
+      <p className="priority">{priorities[task.priority]}</p>
+      <h3 className="task-title"> {task.title}</h3>
+
+      <button className="btn btn-full" onClick={handleClick}>
+        Task details
+      </button>
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+        <h3 className="task-details">Task details</h3>
+        <p>Description: {task.desc || "No description"}</p>
+        <p>Completed: {task.completed ? "Yes" : "No"}</p>
+        <p>Created: {task.created_date.toLocaleDateString()}</p>
+        <p>Complete until: {task.complete_until_date.toLocaleDateString()}</p>
+      </Modal>
     </div>
   );
 };
